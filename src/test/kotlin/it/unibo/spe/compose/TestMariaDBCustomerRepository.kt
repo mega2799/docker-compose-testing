57c0f60279a0eb09ab8285af238db7c47a7b0e8e
『 Matteo Santoro 』 <65976958+mega2799@users.noreply.github.com>
2023-11-25 12:24:14 +0000

    * kotlin is kinda new...

--- src/test/kotlin/it/unibo/spe/compose/TestMariaDBCustomerRepository.kt
+++ src/test/kotlin/it/unibo/spe/compose/TestMariaDBCustomerRepository.kt
@@ -3,10 +3,6 @@ package it.unibo.spe.compose
 import it.unibo.spe.compose.db.MariaDB
 import it.unibo.spe.compose.db.MariaDBConnectionFactory
 import it.unibo.spe.compose.impl.SqlCustomerRepository
-import it.unibo.spe.compose.TestMariaDBCustomerRepository
-import org.junit.AfterClass
-import org.junit.Before
-import org.junit.BeforeClass
 import java.io.File
 import java.lang.IllegalArgumentException
 import java.time.LocalDate
@@ -14,10 +10,12 @@ import java.util.UUID
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import org.junit.AfterClass
+import org.junit.Before
+import org.junit.BeforeClass
 
 class TestMariaDBCustomerRepository {
 
-    
     companion object {
         const val HOST = MariaDB.DEFAULT_HOST
         const val PORT = MariaDB.DEFAULT_PORT
@@ -28,53 +26,80 @@ class TestMariaDBCustomerRepository {
 
         private lateinit var composeFile: File
 
-        @Suppress("UNUSED_PARAMETER")
-        private fun findSubstitution(line : String, vararg pairssss:  Array<out Pair<String, String>>) : String{
-            pairssss.forEach { (p) ->
-                if(line.contains("__"+  p.first  + "__")) {
-                    return line.replace("__"+ p.first + "__", p.second )
+        // @Suppress("UNUSED_PARAMETER")
+        // private fun findSubstitution(
+        //         text: List<String>,
+        //         vararg pairssss: Array<out Pair<String, String>>
+        // ): List<String> {
+        //     return text.map { line ->
+        //             for (p in pairssss) {
+        //                 if (line.contains("__" + p.first + "__")) {
+        //                     line.replace("__" + p.first + "__", p.second)
+        //                 } else {
+        //                     line
+        //                 }
+        //             }
+        //     }
+        // }
+
+        private fun findSubstitution(
+                text: List<String>,
+                vararg pairssss: Array<out Pair<String, String>>
+        ): List<String> {
+            return text.map { line ->
+                var modifiedLine = line
+                pairssss.forEach { pairs ->
+                    pairs.forEach { (first, second) ->
+                        if (modifiedLine.contains("__${first}__")) {
+                            modifiedLine = modifiedLine.replace("__${first}__", second)
+                        }
+                    }
                 }
-            };
-            return line;
+                modifiedLine
+            }
         }
 
         @Suppress("UNUSED_PARAMETER")
         private fun createComposeFile(path: File, vararg assignments: Pair<String, String>): File {
-         val file = File( object {}.javaClass.getResource("docker-compose.yml.template").file )
-         val bufferedReader = file.bufferedReader()
-         val text:List<String> = bufferedReader.readLines()
-         val substitutedLines = text.map{ line -> this.findSubstitution(line, assignments)}
-         val docker = File("docker-compose.yml");
-         docker.createNewFile();
-         docker.writeText(substitutedLines.joinToString("\n"));
-         return docker;
+            val file = File(object {}.javaClass.getResource("docker-compose.yml.template").file)
+            val bufferedReader = file.bufferedReader()
+            val text: List<String> = bufferedReader.readLines()
+            val substitutedLines = this.findSubstitution(text, assignments)
+            val docker = File("docker-compose.yml")
+            docker.createNewFile()
+            docker.writeText(substitutedLines.joinToString("\n"))
+            return docker
         }
 
         /**
-         * Calls `docker compose -f $composeFile $arguments and waits for its completion
-         * If async == false, waits for the command to terminate and asserts that the exit value is 0
+         * Calls `docker compose -f $composeFile $arguments and waits for its completion If async ==
+         * false, waits for the command to terminate and asserts that the exit value is 0
          */
         private fun executeDockerCompose(vararg arguments: String): Process {
-            val command: MutableList<String> = mutableListOf<String>(); //TODO("call docker compose ensuring it will use $composeFile")
+            val command: MutableList<String> =
+                    mutableListOf<String>() // TODO("call docker compose ensuring it will use
+            // $composeFile")
             command.addAll(arguments)
             return ProcessBuilder(command).inheritIO().start().also {
-               // TODO("wait for the process to terminate")
-                //TODO("assert that the exit value is 0 (i.e. make the test fail if the docker compose command fails")
+                // TODO("wait for the process to terminate")
+                // TODO("assert that the exit value is 0 (i.e. make the test fail if the docker
+                // compose command fails")
             }
         }
 
         @BeforeClass
         @JvmStatic
         fun setUpMariaDb() {
-            composeFile = createComposeFile(
-                path = File.createTempFile("docker-compose", ".yml"),
-                "VERSION" to "latest",
-                "ROOT_PASSWORD" to UUID.randomUUID().toString(),
-                "DB_NAME" to DATABASE,
-                "USER" to USER,
-                "PASSWORD" to PASSWORD,
-                "PORT" to "$PORT",
-            )
+            composeFile =
+                    createComposeFile(
+                            path = File.createTempFile("docker-compose", ".yml"),
+                            "VERSION" to "latest",
+                            "ROOT_PASSWORD" to UUID.randomUUID().toString(),
+                            "DB_NAME" to DATABASE,
+                            "USER" to USER,
+                            "PASSWORD" to PASSWORD,
+                            "PORT" to "$PORT",
+                    )
 
             TODO("start the stack in $composeFile")
         }
@@ -97,29 +122,30 @@ class TestMariaDBCustomerRepository {
 
     private val taxCode = TaxCode("CTTGNN92D07D468M")
     private val person = Customer.person(taxCode, "Giovanni", "Ciatto", LocalDate.of(1992, 4, 7))
-    private val person2 = person.clone(birthDate = LocalDate.of(1992, 4, 8), id = TaxCode("CTTGNN92D08D468M"))
+    private val person2 =
+            person.clone(birthDate = LocalDate.of(1992, 4, 8), id = TaxCode("CTTGNN92D08D468M"))
     private val vatNumber = VatNumber(12345678987L)
     private val company = Customer.company(vatNumber, "ACME", "Inc.", LocalDate.of(1920, 1, 1))
 
     @Test
     fun complexTestWhichShouldActuallyBeDecomposedInSmallerTests() {
         assertEquals(
-            expected = emptyList(),
-            actual = repository.findById(taxCode),
+                expected = emptyList(),
+                actual = repository.findById(taxCode),
         )
         assertEquals(
-            expected = emptyList(),
-            actual = repository.findById(vatNumber),
+                expected = emptyList(),
+                actual = repository.findById(vatNumber),
         )
         repository.add(person)
         repository.add(company)
         assertEquals(
-            expected = listOf(person),
-            actual = repository.findById(taxCode),
+                expected = listOf(person),
+                actual = repository.findById(taxCode),
         )
         assertEquals(
-            expected = listOf(company),
-            actual = repository.findById(vatNumber),
+                expected = listOf(company),
+                actual = repository.findById(vatNumber),
         )
         repository.remove(vatNumber)
         repository.update(taxCode, person2)
@@ -127,21 +153,21 @@ class TestMariaDBCustomerRepository {
             repository.remove(taxCode)
         }
         assertEquals(
-            expected = emptyList(),
-            actual = repository.findById(taxCode),
+                expected = emptyList(),
+                actual = repository.findById(taxCode),
         )
         assertEquals(
-            expected = emptyList(),
-            actual = repository.findById(vatNumber),
+                expected = emptyList(),
+                actual = repository.findById(vatNumber),
         )
         assertEquals(
-            expected = listOf(person2),
-            actual = repository.findById(person2.id),
+                expected = listOf(person2),
+                actual = repository.findById(person2.id),
         )
         listOf("Giovanni", "Ciatto").forEach {
             assertEquals(
-                expected = listOf(person2),
-                actual = repository.findByName(it),
+                    expected = listOf(person2),
+                    actual = repository.findByName(it),
             )
         }
     }
